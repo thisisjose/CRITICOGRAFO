e2c0135ce03ffc0218b4318ad2dfc85718ecfb6f
thisisjose <josecinuwu@gmail.com>
2024-01-10 09:07:28 -0700

    .

--- Practica3_JSVE/MainPage.xaml.cs
+++ Practica3_JSVE/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Practica3_JSVE;
 
 namespace Practica3_JSVE
 {
@@ -14,5 +15,79 @@ namespace Practica3_JSVE
         {
             InitializeComponent();
         }
+
+        public void btnCriticar(object sender, EventArgs e)
+        {
+            var nombre = Entry1.Text;
+            string mensage = nombre + " es:";
+
+            if(Hombre.IsChecked == true) 
+            {
+                if (Alto.IsChecked == true)
+                {
+                    mensage += " alto";
+                }
+                if (Feo.IsChecked == true)
+                {
+                    mensage += " feito";
+                }
+                if (Listo.IsChecked == true)
+                {
+                    mensage += " listo";
+                }
+                if(Extrav.IsChecked == true)
+                {
+                    mensage += " extravagante";
+                }
+                if(Raro.IsChecked == true)
+                {
+                    mensage += " rarito";
+                }
+
+                if(Grande.IsChecked == true)
+                {
+                    mensage += " grande";
+                }
+
+            }
+       else if(Mujer.IsChecked == true)
+            {
+                if (Alto.IsChecked == true)
+                {
+                    mensage += " altita";
+                }
+                if (Feo.IsChecked == true)
+                {
+                    mensage += " feita";
+                }
+                if (Listo.IsChecked == true)
+                {
+                    mensage += " lista";
+                }
+                if (Extrav.IsChecked == true)
+                {
+                    mensage += " extravagante";
+                }
+                if (Raro.IsChecked == true)
+                {
+                    mensage += " rarita";
+                }
+
+                if (Grande.IsChecked == true)
+                {
+                    mensage += " grandecita";
+                }
+            }
+            Resultado.Text = mensage;
+        }
+
     }
 }
+
+
+
+
+
+
+
+//Lo que quiero hacer que en el primer Entry que dice escribe, en el radiobutton  x:Name="Hombre" y  x:Name="Mujer", y en los checkbox que hay ALTO, FEO, LISTO, EXTRAVAGANTE, RARO, GRANDE, al momento de seleccionar una o poner un nombre te salga algo asi: Jose es ALTO, RARO Y 
